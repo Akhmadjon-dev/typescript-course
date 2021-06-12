b0d8c54b29b03a5f33f138014fb41df139f53faa
akhmadjon-dev <info.ahmadjon@gmail.com>
2021-06-12 18:06:22 +0500

    finished basics

--- init-ts/app.js
+++ init-ts/app.js
@@ -1,7 +1,7 @@
 function add(a, b) {
     return a + b;
 }
-var num = "3";
+var num = 3;
 var num2 = 3.5;
 var n = add(num, num2);
 console.log(n);
---
+++ init-ts/basics.ts
@@ -0,0 +1,22 @@
+
+
+function add(a: number,b: number, showResult:boolean){
+    if (showResult) {
+        console.log(a+b)
+    }
+    return a+b
+}
+
+let str = 'salom';
+// str = 400
+
+let ism : string;
+
+
+
+const num = 3
+const num2 = 3.5
+const prin = true
+const n = add(num, num2, prin);
+
+console.log(n)
